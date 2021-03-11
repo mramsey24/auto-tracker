47844af835fe56120c2d688cc0138a805d7173a7
Mark Ramsey <mramsey2624@gmail.com>
2021-03-10 20:48:24 -0500

    Add generated Spring Boot files

---
+++ src/main/java/com/mvrsoftware/autotracker/AutoTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mvrsoftware.autotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvrsoftware/autotracker/AutoTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvrsoftware.autotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
